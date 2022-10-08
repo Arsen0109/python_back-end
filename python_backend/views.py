f7e92b9bbc40baf55ce37aca84cd1c8a70a622d9
Arsen0109 <balasarseniy777@gmail.com>
2022-10-08 20:19:36 +0300

    adding functionality for creating and getting notes

--- python_backend/views.py
+++ python_backend/views.py
@@ -16,8 +16,7 @@ CATEGORIES = [
 USERS = [
     {
         "id": user_id,
-        "name": "Vasyl",
-
+        "name": "Vasyl"
     }
 ]
 
@@ -30,6 +29,13 @@ NOTES = [
         "date_of_creating": datetime.date.today()
     }
 ]
+
+
+def validation(key, value, arr):
+    for i in arr:
+        if i[key] == value:
+            return True
+    return False
 
 
 @app.route("/")
@@ -46,10 +52,9 @@ def create_user():
     try:
         request_data["name"] = request.get_json()["name"]
     except:
-        request_data["name"] = "User" + str(user_id)
+        return "Error bad request"
 
     USERS.append(request_data)
-    print(USERS)
     return request_data
 
 
@@ -62,10 +67,26 @@ def create_category():
     try:
         request_data["title"] = request.get_json()["title"]
     except:
-        request_data["title"] = "Title" + str(user_id)
-
+        return "Error bad request"
     CATEGORIES.append(request_data)
-    print(CATEGORIES)
+    return request_data
+
+
+@app.route("/note", methods=["POST"])
+def create_note():
+    request_data = request.get_json()
+    global note_id
+    note_id += 1
+    try:
+        if not (validation("id", request.get_json()["user_id"], USERS) and validation("id", request.get_json()["category_id"], CATEGORIES)):
+            return "Error, user or category is not found"
+        request_data["id"] = note_id
+        request_data["date_of_creating"] = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        request_data["price"] = request.get_json()["price"]
+    except:
+        return "Error bad request"
+
+    NOTES.append(request_data)
     return request_data
 
 
@@ -77,3 +98,8 @@ def get_user():
 @app.route("/categories")
 def get_categories():
     return jsonify({"categories": CATEGORIES})
+
+
+@app.route("/notes")
+def get_notes():
+    return jsonify({"notes": NOTES})
